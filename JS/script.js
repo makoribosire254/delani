bdac56c456c6a1002c4fb2a2b85fe7511475da20
KOPITE007 <makoribosire254@gmail.com>
2021-06-20 18:25:38 +0300

    making changes on the script and style sheet

--- JS/script.js
+++ JS/script.js
@@ -1,23 +1,30 @@
-var name;
-var email;
+// script contact form and input
 
 function fn1(){
-  name=document.forms["myForm"]["name"].value;
-  email=document.forms["myForm"]["email"].value;
+name=document.forms["myForm"]["name"].value;
+email=document.forms["myForm"]["email"].value;
 if(name==""){
   alert("Enter Name");
 }
 if(email==""){
   alert("Enter Email");
 }
-alert(name.value + ""+ "We've received Your message. Thank you for contacting us");
 }
+console.log("Name and email entered");
 
 //Toggling the description upon clicking the services_icons
-
 $(document).ready(function(){
-$(.clickable).click(function(){
-  $("#p_hide").toggle();
-  $("#p_hide").toggle();
+$("div#p_hide1").hide();
+$("#dw1,#p_hide1").click(function(){
+  $("#p_hide1,#dw1").toggle();
+});
+
+$("div#p_hide2").hide();
+$("#dw2,#p_hide2").click(function(){
+  $("#dw2,#p_hide2").toggle();
+});
+  $("div#p_hide3").hide();
+  $("#dw3,#p_hide3").click(function(){
+    $("#dw3,#p_hide3").toggle();
 });
 });
